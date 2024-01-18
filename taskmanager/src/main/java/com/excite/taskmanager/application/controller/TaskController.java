d5b983824adac157851869ed8cddc3b98a6bcc79
AsahiSaSaki <asahi.sasaki@excite-software.co.jp>
2024-01-18 15:38:16 +0900

    タスク取得406エラー

--- taskmanager/src/main/java/com/excite/taskmanager/application/controller/TaskController.java
+++ taskmanager/src/main/java/com/excite/taskmanager/application/controller/TaskController.java
@@ -3,11 +3,13 @@ package com.excite.taskmanager.application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.excite.taskmanager.domain.object.TaskObject;
 import com.excite.taskmanager.domain.service.TaskService;
 
 
@@ -17,6 +19,19 @@ public class TaskController {
 
     @Autowired
     private TaskService taskService;
+
+
+    /**
+     * タスク取得
+     *
+     * @param id 
+     */
+    @GetMapping("taskmanager/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public TaskObject getTaskById(@PathVariable("id") Integer id) {
+        return taskService.getTaskById(id);
+      
+    }
 
     /**
      * タスク削除
