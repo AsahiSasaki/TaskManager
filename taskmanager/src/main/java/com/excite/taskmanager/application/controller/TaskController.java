f97add54498f3e808b5b9325c3f29e92836d1240
AsahiSaSaki <asahi.sasaki@excite-software.co.jp>
2024-01-19 15:35:41 +0900

    タスク作成、タスク更新API

--- taskmanager/src/main/java/com/excite/taskmanager/application/controller/TaskController.java
+++ taskmanager/src/main/java/com/excite/taskmanager/application/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,12 +54,23 @@ public class TaskController {
     /**
      * タスク作成
      *
-     * @param id 
+     * @param 
      */
     @PostMapping("taskmanager")
     public void createTask(@RequestBody TaskBody taskBody) {
         TaskObject reqTaskObject = modelMapper.map(taskBody, TaskObject.class);
         taskService.createTask(reqTaskObject);
+    }
+
+    /**
+     * タスク更新
+     *
+     * @param 
+     */
+    @PutMapping("taskmanager")
+    public void updateTask(@RequestBody TaskBody taskBody) {
+        TaskObject reqTaskObject = modelMapper.map(taskBody, TaskObject.class);
+        taskService.updateTask(reqTaskObject);
     }
 
     /**
